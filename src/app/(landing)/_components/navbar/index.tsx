f7251b2ae4714a55b135639c9688ea47586bfb5f
Abdelrahman_Abass <abdoabass400@gmail.com>
2024-09-23 12:27:19 +0300

    build : the first structure

---
+++ src/app/(landing)/_components/navbar/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Menu from './menu'
+
+type Props = {}
+
+const LandingPageNavbar = (props: Props) => {
+  return (
+    // <div className='w-full sticky top-0 flex justify-between items-center py-5 z-50'>
+    //   <p className='font-bold tex-2xl'>Grouple.</p> 
+    //   <Menu orientation="desktop" /> 
+    // </div>
+    <h1 className='w-full sticky top-0 flex justify-between items-center py-5 z-50'>Grouple.</h1>
+  )
+}
+
+export default LandingPageNavbar
---
+++ src/app/(landing)/_components/navbar/menu.tsx
@@ -0,0 +1,34 @@
+"use client"
+
+import { Card, CardContent } from "@/components/ui/card"
+import { useNavigation } from "@/hooks/navgation/useNavugation"
+
+
+type MenuProps = {
+    orientation: "mobile" | "desktop"
+}
+
+const Menu = ({ orientation }: MenuProps) => {
+    const { section, onSetSection } = useNavigation()
+
+    switch (orientation) {
+        case "desktop":
+            return <Card className="bg-themeGray border-themeGray bg-clip-padding backdrop--blur__safari backdrop-filter backdrop-blur-2xl bg-opacity-60 p-1 lg:flex hidden rounded-xl">
+                <CardContent className="p-0 flex gap-2">
+                    
+                </CardContent>
+            </Card>
+
+        case "mobile":
+            return <div>
+
+            </div>
+        default:
+            return <></>
+    }
+    return (
+        <div>menu</div>
+    )
+}
+
+export default Menu
